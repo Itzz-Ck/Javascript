4c5196f318f0321c478991c7996363311d8e4de7
Itzz-Ck <146458372+Itzz-Ck@users.noreply.github.com>
2023-12-17 21:15:19 +0000

    Just Started

---
+++ .devcontainer/Level0/TypeConversion.js
@@ -0,0 +1,23 @@
+let a0=10
+let a1="Ck"
+let a2="10a"
+let a3=true
+let a4=false
+let a5=undefined
+let a6=null
+
+//NaN => Not a Number
+
+// 1=>true 0=>false   ""=>false "hbrrf"=>true
+
+let b0=Number(a0)
+let b1=Number(a1)
+let b2=Number(a2)
+let b3=Number(a3)
+let b4=Number(a4)
+let b5=Number(a5)
+let b6=Number(a6)
+
+console.log([b0,b1,b2,b3,b4,b5,b6])
+
+console.log([typeof(b0),typeof(b1),typeof(b2),typeof(b3),typeof(b4),typeof(b5),typeof(b6)])
---
+++ .devcontainer/Level0/test.js
@@ -0,0 +1,11 @@
+const a=10
+let b=5
+var c=6
+d=9
+
+/*  const is like constant its value  cant be modified if once initiated 
+    
+    Never use "var" due to some scope related issues 
+*/
+console.log(a)
+console.table([a,b,c])
